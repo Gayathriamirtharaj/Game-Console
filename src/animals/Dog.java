e3fb071baf6ac13338c40ff59a0db9eff3b25516
Gayathriamirtharaj <gayathriamirtharaj@gmail.com>
2020-07-15 14:48:03 +0530

    second commit

--- src/animals/Dog.java
+++ src/animals/Dog.java
@@ -2,21 +2,31 @@ package animals;
 
 import wild.WildAnimals;
 
-public class Dog extends WildAnimals implements playable{
+public class Dog extends Animal implements playable{
     String type="dog";
     String name="";
     int no_of_legs=4;
     public Dog(String dogname)
     {
-        name=dogname;
+        super(dogname);
+
     }
+    public String getName()
+    {
+        return(this.name);
+    }
+    public String getType(){return type;}
+
+
     public void eat(){
         System.out.println("Am an omnivore");
     }
     public void speak(){
-        System.out.println("HI!! AM "+name +"and i have "+no_of_legs);
+        System.out.println("HI!! AM "+getName()+"and i have "+no_of_legs);
     }
     public void play(){
+        getType();
+        getName();
         String playing="Throw me a ball and i can fetch";
         speak();
         System.out.println(playing);
